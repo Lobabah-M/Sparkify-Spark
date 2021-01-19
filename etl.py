278ad0821c47d1db142ac0bb2b57a84531fcde77
Lobabah-M <lobabah.alalawiyat@gmail.com>
2021-01-19 20:04:33 +0300

    Implement process_song_data in etl.py
    Complete the implementation of the function proces_song_data to do the following
    - Read all the song data from S3 into a dataframe
    - Extract columns to create songs and artists table
    - Write songs and artists tables to parquet files in S3

--- etl.py
+++ etl.py
@@ -24,7 +24,46 @@ def create_spark_session():
 
 
 def process_song_data(spark, input_data, output_data):
+    """
+    - reads all the song data from S3 into a dataframe
+    - extracts columns to create songs and artists table
+    - writes songs and artists tables to parquet files in S3
+    """
+    # get filepath to song data file
+    song_data = input_data+"song_data/*/*/*/*.json"
+    
+    # read song data files
+    df = spark.read.json(song_data)
 
+    # extract columns to create songs table
+    df.createOrReplaceTempView("song_data")
+    songs_table = spark.sql("""SELECT DISTINCT song_id, title, artist_id, year, duration
+                               FROM song_data
+                               WHERE song_id IS NOT NULL""")
+    
+    # write songs table to parquet files partitioned by year and artist
+    (songs_table
+     .write
+     .mode('overwrite')
+     .partitionBy("year", "artist_id")
+     .parquet(output_data+'songs/')
+    )
+
+    # extract columns to create artists table
+    artists_table = spark.sql("""SELECT DISTINCT artist_id, 
+                                                 artist_name as name, 
+                                                 artist_location as location, 
+                                                 artist_latitude as latitude, 
+                                                 artist_longitude as longitude
+                                 FROM song_data
+                                 WHERE artist_id IS NOT NULL""") 
+    
+    # write artists table to parquet files
+    (artists_table
+     .write
+     .mode('overwrite')
+     .parquet(output_data+'artists/')
+    )
 
 
 def process_log_data(spark, input_data, output_data):
